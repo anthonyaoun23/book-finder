e41a420d25b93f1ae52c6417337e872617f6133c
Anthony Aoun <anthony.aoun1@hotmail.com>
2025-03-06 14:26:53 -0500

    Add rate limit to upload ctrl

--- apps/api/src/upload/upload.module.ts
+++ apps/api/src/upload/upload.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        throttlers: [
+          {
+            ttl: Number(configService.getOrThrow('UPLOAD_RATE_TTL')) * 1000,
+            limit: Number(configService.getOrThrow('UPLOAD_RATE_LIMIT')),
+          },
+        ],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [UploadController],
-  providers: [UploadService],
+  providers: [
+    UploadService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class UploadModule {}
